207d64f302121d652a6f87c34a97cc3e8a1fde3d
Stefan Horning <stefanhorning@iMac-von-Stefan.fritz.box>
2023-02-12 12:28:30 +0100

    JSON

---
+++ script.js
@@ -0,0 +1,43 @@
+let questions =[
+    {
+        'question': 'Wofür ist Javascriprt da?',
+        'answer_1': 'Für das Grundgerüst einer Website',
+        'answer_2': 'Für die logik einer einers Programmes',
+        'answer_3': 'Was ist Javascript',
+        'answer_4': 'Um etwas zu Stylen',
+        'right_answer': 2
+    },
+    {
+        'question': 'Was ist <b>keine</b> Programmiersprache?',
+        'answer_1': 'php',
+        'answer_2': 'Javascript',
+        'answer_3': 'Zoom4',
+        'answer_4': 'Java',
+        'right_answer': 3
+    },
+    {
+        'question': 'Welche Programmiersprache schreibt man wie ein Kochrezept?',
+        'answer_1': 'Chef',
+        'answer_2': 'Javascript',
+        'answer_3': 'Python',
+        'answer_4': 'C++',
+        'right_answer': 1
+    },
+    {
+        'question': 'Wie Zählt ein Informatiker?',
+        'answer_1': '1,2,3,4,5...',
+        'answer_2': '-1,0,1,2,3,4,5...',
+        'answer_3': '0,1,2,3,4,5...',
+        'answer_4': 'Garnicht',
+        'right_answer': 3
+    },
+    {
+        'question': 'Was ist ChatGPT',
+        'answer_1': 'Eine TV-Show',
+        'answer_2': 'Eine Anderebesvhreibung für dein Quell-Code',
+        'answer_3': 'Ein sehr guter PC',
+        'answer_4': 'Eine Künstliche Intelligenz',
+        'right_answer': 4
+    }
+];
+
